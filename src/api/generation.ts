59458427f20c04ff1874df3feaf111f50fa8cd2f
whtfckV <104555352+whtfckV@users.noreply.github.com>
2024-09-23 16:11:52 +0700

    change linter

--- src/api/generation.ts
+++ src/api/generation.ts
@@ -1,6 +1,6 @@
-import type { ApiError, ApiResponse } from "@/types/api"
+import type { ApiError, ApiResponse } from '@/types/api'
 import { Api, Post } from "."
-import type { GenerateRequestBody, GenerateResponse } from "@/types/generation"
+// import type { GenerateRequestBody, GenerateResponse } from "@/types/generation"
 
 export const generate = async (body: GenerateRequestBody): Promise<ApiResponse<GenerateResponse> | ApiError> => {
   return await Api.post<GenerateResponse, GenerateRequestBody>(Post.generation, body)
