2aee874880b33d6dbe5f60ca626ca2e84ed2d19f
whtfckV <104555352+whtfckV@users.noreply.github.com>
2024-09-25 13:26:00 +0700

    bag with formatting

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,46 +1,46 @@
 <script setup lang="ts">
 // import AuthForm from "@/components/authorization/AuthForm.vue";
-import { token } from "@/api/auth";
-import { useRouter } from "vue-router";
+  import { token } from '@/api/auth'
+  import { useRouter } from 'vue-router'
 
-const router = useRouter();
+  const router = useRouter()
 
-const data = ref ({
-  login: "1",
-  password: "2"
-})
+  const data = ref({
+    login: '1',
+    password: '2',
+  })
 
-const getToken = async (login: string, password: string) => {
-  const data = new URLSearchParams({
-    username: login,
-    password: password
-  });
-  return await token(data)
-}
-
-const handleLogin = async (loginData: any) => {
-  // console.log(loginData.login, loginData.password)
-  const response = await getToken(loginData.login, loginData.password);
-
-  if (response.success) {
-    const token = response.data;
-    console.log(token);
-
-    if (token.access_token) {
-      localStorage.setItem('authToken', token.access_token);
-      router.push({ name: '/' });
-    }
-  } else {
-    console.error('Ошибка авторизации:', response.error);
+  const getToken = async (login: string, password: string) => {
+    const data = new URLSearchParams({
+      username: login,
+      password,
+    })
+    return await token(data)
   }
-};
+
+  const handleLogin = async (loginData: any) => {
+    // console.log(loginData.login, loginData.password)
+    const response = await getToken(loginData.login, loginData.password)
+
+    if (response.success) {
+      const token = response.data
+      console.log(token)
+
+      if (token.access_token) {
+        localStorage.setItem('authToken', token.access_token)
+        router.push({ name: '/' })
+      }
+    } else {
+      console.error('Ошибка авторизации:', response.error)
+    }
+  }
 
 </script>
 
 <template>
   <div class="auth">
     <div class="auth__container">
-      <AuthForm v-model:data=data @login-success="handleLogin" />
+      <AuthForm v-model:data="data" @login-success="handleLogin" />
     </div>
   </div>
 </template>
