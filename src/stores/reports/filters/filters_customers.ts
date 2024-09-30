e3ffce1b91068e9539e0e41d19cf38b2f61c03da
whtfckV <104555352+whtfckV@users.noreply.github.com>
2024-09-30 13:21:10 +0700

    change filters

--- src/stores/reports/filters/filters_customers.ts
+++ src/stores/reports/filters/filters_customers.ts
@@ -1,22 +1,37 @@
-import { getCustomersFilters } from '@/api/reports/filters'
-import { FilterEntityTranformed } from '@/types/reports/filters'
+import { getCustomersFilters, getPartnersFilters, getProductsFilters } from '@/api/reports/filters'
+import { ApiError, ApiResponse } from '@/types/api'
+import { FilterEntity, Filters, FiltersTypes } from '@/types/reports/filters'
 import { defineStore } from 'pinia'
 
+const FiltersMethods: Record<FiltersTypes, () => Promise<ApiResponse<FilterEntity[]> | ApiError>> = {
+  customers: getCustomersFilters,
+  products: getProductsFilters,
+  partners: getPartnersFilters,
+}
+
 export const useFiltersCustomersStore = defineStore('costumers', () => {
-  const filters = reactive<FilterEntityTranformed[]>([])
+  const filters = reactive<Filters>({
+    partners: [],
+    products: [],
+    customers: [],
+  })
   const isLoading = ref(false)
   const error = ref()
 
-  const getFilter = async () => {
+  const updateFilter = (data: FilterEntity[], filterType: FiltersTypes) => {
+    filters[filterType].splice(0)
+    const transformedData = data.map(item => ({
+      value: item.id,
+      title: item.name,
+    }))
+    filters[filterType].push(...transformedData)
+  }
+
+  const getFilter = async (filterType: FiltersTypes) => {
     try {
-      const response = await getCustomersFilters()
+      const response = await FiltersMethods[filterType]()
       if (response.success) {
-        filters.splice(0)
-        const transformedData = response.data.map(item => ({
-          value: item.id,
-          title: item.name,
-        }))
-        filters.push(...transformedData)
+        updateFilter(response.data, 'customers')
       } else {
         error.value = response.error
       }
