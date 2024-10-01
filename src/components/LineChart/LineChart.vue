14d2268b9da5959e4622f78a1dc6bd27c6160062
whtfckV <104555352+whtfckV@users.noreply.github.com>
2024-10-01 13:49:01 +0700

    add chart animation

--- src/components/LineChart/LineChart.vue
+++ src/components/LineChart/LineChart.vue
@@ -26,8 +26,40 @@
     tension: 0.4,
   })
 
+  let delayed: boolean = false
+
   const options = {
     responsive: true,
+    interaction: {
+      intersect: false,
+    },
+    animation: {
+      onComplete: () => {
+        delayed = true
+      },
+      delay: (context: any) => {
+        let delay = 0
+        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+          delay = context.dataIndex * 100 + context.datasetIndex * 100
+        }
+        return delay
+      },
+    },
+    // scales: {
+    //   x: {
+    //     display: true,
+    //     title: {
+    //       display: true,
+    //     },
+    //   },
+    //   y: {
+    //     display: true,
+    //     title: {
+    //       display: true,
+    //       text: 'Value',
+    //     },
+    //   },
+    // },
     // maintainAspectRatio: false,
   }
 
